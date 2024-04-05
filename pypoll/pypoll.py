f9f82313e6e747db19c95e0af53505d3f3b84a93
makram <m_karabibar@hotmail.com>
2024-04-05 09:40:09 -0400

    add codes to pypoll

---
+++ pypoll/pypoll.py
@@ -0,0 +1,37 @@
+#The total number of votes cast
+#A complete list of candidates who received votes
+#The percentage of votes each candidate won
+#The total number of votes each candidate won
+#The winner of the election based on popular vote
+import os 
+file_path = os.path.join('..','pypoll','Resources','election_data.csv')
+import csv
+
+with open(file_path,'r') as election:
+    csv_reader = csv.reader(election,delimiter=',')
+    header = next(election)
+    candidates_list = []
+    total_votes=0
+    candidate_count = [0,0,0]
+    candidate_percentagge_vote = [0,0,0]
+    for row in csv_reader:
+        total_votes += row.count(row[0])
+        if row[2] not in candidates_list:
+            candidates_list.append(row[2])
+        for i in range(len(candidates_list)):
+            if candidates_list[i]==row[2]:
+                candidate_count[i]+=1
+        for j in range(len(candidate_count)):
+            candidate_percentagge_vote[j] = round((candidate_count[j]/total_votes)*100,2)
+
+            
+    
+
+    print(f'The total votes is {total_votes}')
+    print(f'The candidates are : {candidates_list}')
+    print(f'The candidates received total votes as following :{candidate_count}')
+    print(f'The total percentage receive by candidate is : {candidate_percentagge_vote}')
+    print(f'The winner is {candidates_list[candidate_count.index(max(candidate_count))]} with {candidate_percentagge_vote[candidate_count.index(max(candidate_count))]} %')
+
+
+
